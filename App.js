ae19f14f5c8fb4da29d572f092f235d79289b0a4
Matthew Landis <matthewlandis5@gmail.com>
2023-01-18 16:32:04 -0500

    Adding intial React application code

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+
+const Cat = () => {
+  const [text, setText] = useState('...');
+
+  return (
+    <View>
+      <Text>Hello, I am {text}</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="Name me!"
+        onChangeText={newText => setText(newText)}
+      />
+    </View>
+  );
+};
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {Cat()}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
