42019f91e639ebacaf34c04b0467f6e6e9b42440
Matthew Landis <matthewlandis5@gmail.com>
2023-01-21 22:15:49 -0500

    Add Firebase functionality

--- App.tsx
+++ App.tsx
@@ -1,23 +1,84 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import { initializeApp } from "firebase/app";
+import {getDatabase, ref, get, child, set, update, remove} from "firebase/database";
+import morseConversion from './letter-morse-conversion.json';
+import firebaseConfig from './config/firebase-config.json';
 
-const Cat = () => {
-  const [text, setText] = useState('...');
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const convertTextToMorse = (text: String) => {
+  //TODO: Convert text to morse
+  //TODO: Vibrate phone with morse
+};
+
+
+const Elements = () => {
+  const [text, setText] = useState<String>('...');
+  const [morse, setMorse] = useState<JSON | null>();
+
+  if (morse == null) {
+    get(child(ref(db), 'morse')).then((snapshot) => {
+      if (snapshot.exists()) {
+        setMorse(snapshot.val());
+        console.log('Morse data downloaded from DB')
+      } else {
+        alert('No morse data available');
+      }
+    }).catch((error) => {
+      alert('Error getting data from DB: ' + error);
+    });
+  }
 
   return (
     <View>
-      <Text>Hello, I am {text}</Text>
+      <Text>What would you like to hear in morse?</Text>
       <TextInput
         style={{
           height: 40,
           borderColor: 'gray',
           borderWidth: 1,
         }}
-        defaultValue="Name me!"
+        defaultValue="Hello"
         maxLength={40}
         onChangeText={newText => setText(newText)}
       />
+      <Button
+        onPress={() => {
+          convertTextToMorse(text);
+        }}
+        title="Convert"
+      />
+
+      {/* UPLOAD
+      <Button
+        onPress={() => {
+          set(ref(db, 'morse'), morseConversion).catch((error) => {
+            alert('Error Uploading! ' + error);
+          }).then(() => {
+            alert('Successful Uploading!');
+          });
+        }}
+        title="Upload"
+      />*/}
+      {/* DOWNLOAD
+      <Button
+        onPress={() => {
+          get(child(ref(db), 'morse')).then((snapshot) => {
+            if (snapshot.exists()) {
+              alert(JSON.stringify(snapshot.val()));
+            } else {
+              alert('No data available');
+            }
+          }).catch((error) => {
+            alert('Error getting data: ' + error);
+          });
+        }
+        }
+        title="Download"
+      />*/}
     </View>
   );
 };
@@ -25,7 +86,7 @@ const Cat = () => {
 export default function App() {
   return (
     <View style={styles.container}>
-      {Cat()}
+      {Elements()}
       <StatusBar style="auto" />
     </View>
   );
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Welcome to the First Coding Assignment for CS 4261 MAS
+
+### Steps to get up and running
+1. Clone repo
+2. Add firebase config file as `/config/firebase-config.json` (it was omitted to security reasons)
+3. Install Expo from npm
+4. Run `expo start` in the command line
+5. Connect to mobile device either through usb or expoGo
+
+
